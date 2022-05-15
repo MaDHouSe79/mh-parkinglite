8311bcb0c3a46798d1c1dc5807b10a9d1abb2dcd
MaDHouSe79 <madhouse1979@gmail.com>
2022-05-15 10:24:18 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -73,7 +73,7 @@ local function RefreshVehicles(src)
                 }
                 if QBCore.Functions.GetPlayer(src) ~= nil and QBCore.Functions.GetPlayer(src).PlayerData.citizenid == v.citizenid then
                     if not Config.ImUsingOtherKeyScript then
-                        TriggerClientEvent('vehiclekeys:client:SetOwner', QBCore.Functions.GetPlayer(src), v.plate)
+                        TriggerEvent('qb-vehiclekeys:server:AcquireVehicleKeys', v.plate)
                     end
                 end
             end
