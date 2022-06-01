5831819e9d9897ac941ae6aca20e98d330891789
MaDHouSe79 <madhouse1979@gmail.com>
2022-06-01 16:20:58 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -256,3 +256,32 @@ end)
 
 -- Refresh vehicles.
 RegisterServerEvent('qb-parking:server:refreshVehicles', function(parkingName) RefreshVehicles(source) end)
+
+
+local function checkVersion(err, responseText, headers)
+    curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
+    if responseText == nil then
+        print("^1"..resourceName.." check for updates failed ^7")
+        return
+    end
+    if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+        updateavail = true
+        print("\n^1----------------------------------------------------------------------------------^7")
+        print(resourceName.." is outdated, latest version is: ^2"..responseText.."^7, installed version: ^1"..curVersion.."^7!\nupdate from https://github.com"..updatePath.."")
+        print("^1----------------------------------------------------------------------------------^7")
+    elseif tonumber(curVersion) > tonumber(responseText) then
+        print("\n^3----------------------------------------------------------------------------------^7")
+        print(resourceName.." git version is: ^2"..responseText.."^7, installed version: ^1"..curVersion.."^7!")
+        print("^3----------------------------------------------------------------------------------^7")
+    else
+        print("\n"..resourceName.." is up to date. (^2"..curVersion.."^7)")
+    end
+end
+
+if Config.CheckForUpdates then
+    Citizen.CreateThread( function()
+        updatePath = "/MaDHouSe79/qb-parkinglite"
+        resourceName = "("..GetCurrentResourceName()..")"
+        PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version", checkVersion, "GET")
+    end)
+end
