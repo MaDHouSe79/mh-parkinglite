6f4ef9ab34af08b9dac870a2b29ebc13c6491ce2
MaDHouSe79 <madhouse1979@gmail.com>
2022-06-04 01:13:42 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,45 +1,47 @@
 Config                           = {}
----------
 
-Config.CheckForUpdates           = true
+-- ðŸ‘‡ Sometime the vehicle spawn on top of each other and to avoid this you can use this delay below.
+Config.UseSpawnDelay             = true       -- ðŸ‘‰ Default false, if your vehicles spawn on top of each other, set this to true
+Config.DeleteDelay               = 1500       -- ðŸ‘‰ Default 500, a delay for spawning in a other vehicle. (works only if Config.UseSpawnDelay = true)
+Config.FreezeDelay               = 50         -- ðŸ‘‰ Default 10, a sort delay for freezeing a vehicle. (works only if Config.UseSpawnDelay = true)
 
-Config.UseSpawnDelay             = true      -- 👉 Default false, if your vehicles spawn on top of each other, set this to true
-Config.DeleteDelay               = 1500      -- 👉 Default 500, a delay for spawning in a other vehicle. (works only if Config.UseSpawnDelay = true)
-Config.FreezeDelay               = 50        -- 👉 Default 10, a sort delay for freezeing a vehicle. (works only if Config.UseSpawnDelay = true)
-Config.DisplayDistance           = 20.0      -- 👉 Distence to see text above parked vehicles (player dependent)
----------
-Config.KeyBindButton             = "F5"      -- 👉 If you want to change the drive and park button. (you must use /binds for this)
-Config.ParkingButton             = 166       -- 👉 F5 (vehicle exit and or park)
----------
-Config.UseStopSpeedForPark       = true      -- 👉 Default true
-Config.MinSpeedToPark            = 0.9       -- 👉 Default 1 the min speed to park
----------
-Config.PlaceOnGroundRadius       = 100.0     -- 👉 lower wil limit the distance of placeing vehicles on the ground.
-Config.ResetState                = 1         -- 👉 1 is stored in garage, 2 is police impound. 
----------
-Config.UseParkingSystem          = true      -- 👉 Auto turn on when server is starting. (default true)
-Config.UseParkedVehicleNames     = false     -- 👉 Default is false, if you want to see names just type /park-names on/off if you set this to true it is auto on 
-Config.UseOwnerNames             = false     -- 👉 If you want to use owner names above parked vehicles
+Config.CheckForUpdates           = true       -- ðŸ‘‰ If you want to stay updated keep it on true.
+Config.DisplayDistance           = 20.0       -- ðŸ‘‰ Distence to see text above parked vehicles (player dependent)
 
-Config.UseParkingBlips           = true      -- 👉 If you want to have parking blips on the map (player dependent)
+Config.KeyBindButton             = "F5"       -- ðŸ‘‰ If you want to change the drive and park button. (you must use /binds for this)
+Config.parkingButton             = 166        -- ðŸ‘‰ F5 (vehicle exit and or park)
+Config.useRoleplayName           = true       -- ðŸ‘‰ If you want to use Roleplay name above the cars (firstname lastname) set this on true
 
-Config.UseTargetEye              = true      -- 👉 If you want to use target
-Config.InteractDistance          = 5.0       -- 👉 Interact distance.
+Config.UseStopSpeedForPark       = true       -- ðŸ‘‰ Default true
+Config.MinSpeedToPark            = 1          -- ðŸ‘‰ Default 1 the min speed to park
 
--- Vip Or All players can park.
-Config.UseForVipOnly             = false     -- 👉 If you only want to use vip set this to true: NOTE (Config.UseMaxParkingOnServer and Config.UseMaxParkingPerPlayer to false)
-Config.UseMaxParkingOnServer     = true      -- 👉 If you want to limit the parking on the server
-Config.MaxServerParkedVehicles   = 25        -- 👉 Max allowed to park on server
-Config.UseMaxParkingPerPlayer    = true      -- 👉 If you want to limit players with a amount of parking vehicles
-Config.MaxStreetParkingPerPlayer = 1         -- 👉 Max allowed pakring vehivles per player
----------
+-- ðŸ‘‡ Default 2, this reset the state of the vehicles, to check if the vehicle is still parked outside, if not it will reset the state      
+Config.PlaceOnGroundRadius       = 100.0      -- ðŸ‘‰ lower wil limit the distance of placeing vehicles on the ground.
+Config.ResetState                = 1          -- ðŸ‘‰ 1 is stored in garage, 2 is police impound. 
 
+-- ðŸ‘‡ Base config when the server start, this is the default settings
+Config.UseParkingSystem          = true       -- ðŸ‘‰ Auto turn on when server is starting. (default true)
+Config.UseParkedVehicleNames     = true       -- ðŸ‘‰ Default is false, if you want to see names just type /park-names on/off if you set this to true it is auto on 
+Config.UseOwnerNames             = true
+Config.UseParkingBlips           = true
+
+Config.UseMaxParkingOnServer     = true       -- ðŸ‘‰ if you want to limit the parking on the server
+Config.MaxServerParkedVehicles   = 25         -- ðŸ‘‰ max allowed to park on server
+
+Config.UseMaxParkingPerPlayer    = true       -- ðŸ‘‰ if you want to limit players with a amount of parking vehicles
+Config.MaxStreetParkingPerPlayer = 5          -- ðŸ‘‰ max allowed parking vehicles per player
+
+Config.UseTargetEye              = true
+Config.InteractDistance          = 5.0
+
+Config.UseForVipOnly             = false
+-- ðŸ‘‡ change this to your own commands
 Config.Command = {
-    park         = 'park',                   -- 👉 User/Admin permission
-    parknames    = 'park-names',             -- 👉 User/Admin permission
-    addvip       = 'park-addvip',            -- 👉 Admin permission
-    removevip    = 'park-removevip',         -- 👉 Admin permission
+    park         = 'park',                    -- ðŸ‘‰ User/Admin permission
+    parknames    = 'park-names',              -- ðŸ‘‰ User/Admin permission
+    addvip       = 'park-addvip',             -- ðŸ‘‰ Admin permission
+    removevip    = 'park-removevip',          -- ðŸ‘‰ Admin permission
 }
 
--- Do not change this below...
+-- ðŸ‘‡ Dont change this, you will not be able to park if you change this...
 Config.ParkingLocation = {x = 232.11, y = -770.14, z = 0.0, w = 900.10, s = 99999099.0}
