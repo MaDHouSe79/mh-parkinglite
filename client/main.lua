548d335ded8e871d198e1835c23848c6b0773132
MaDHouSe79 <madhouse1979@gmail.com>
2022-07-04 19:53:51 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -116,7 +116,9 @@ local function LoadEntity(vehicleData, type)
     local model = vehicleData.vehicle.props.model
     QBCore.Functions.LoadModel(vehicleData.vehicle.props.model)
     VehicleEntity = CreateVehicle(vehicleData.vehicle.props.model, vehicleData.vehicle.location.x, vehicleData.vehicle.location.y, vehicleData.vehicle.location.z - 0.1, vehicleData.vehicle.location.w, false)
-    QBCore.Functions.SetVehicleProperties(VehicleEntity, vehicleData.vehicle.props)
+    local properties = json.decode(vehicleData.vehicle.props)
+    QBCore.Functions.SetVehicleProperties(VehicleEntity, properties)
+    --QBCore.Functions.SetVehicleProperties(VehicleEntity, vehicleData.vehicle.props)
     SetVehicleEngineOn(VehicleEntity, false, false, true)
     SetVehicleDoorsLocked(VehicleEntity, 2)
     if type == 'server' then
@@ -308,7 +310,9 @@ local function MakeVehicleReadyToDrive(vehicle, warp)
     QBCore.Functions.LoadModel(vehicle.props.model)
     local VehicleEntity = CreateVehicle(vehicle.props.model, vehicle.location.x, vehicle.location.y, vehicle.location.z, vehicle.location.w, true)
     if warp then TaskWarpPedIntoVehicle(PlayerPedId(), VehicleEntity, -1) end
-    QBCore.Functions.SetVehicleProperties(VehicleEntity, vehicle.props)
+    local properties = json.decode(vehicle.props)
+    QBCore.Functions.SetVehicleProperties(VehicleEntity, properties)
+    --QBCore.Functions.SetVehicleProperties(VehicleEntity, vehicle.props)
     RequestCollisionAtCoord(vehicle.location.x, vehicle.location.y, vehicle.location.z)
     SetVehicleOnGroundProperly(VehicleEntity)
     FreezeEntityPosition(VehicleEntity, false)
@@ -607,3 +611,429 @@ CreateThread(function()
         Wait(Config.ForceGroundenInMilSec)
     end
 end)
+
+function SetVehicleModifications(vehicle, props)-- Apply all modifications to a vehicle entity
+    if DoesEntityExist(vehicle) then
+		SetVehicleModKit(vehicle, 0)
+		-- plate:
+		if props.plate then
+            SetVehicleNumberPlateText(vehicle, props.plate)
+        end
+        if props.plateIndex then
+            SetVehicleNumberPlateTextIndex(vehicle, props.plateIndex)
+        end
+		-- lockStatus:
+		if props.lockstatus then
+			SetVehicleDoorsLocked(vehicle, props.lockstatus)
+		end
+		-- colours:
+		if props.color1 and props.color2 then
+			SetVehicleColours(vehicle, props.color1, props.color2)
+		end
+		if props.customprimarycolor then
+			SetVehicleCustomPrimaryColour(vehicle, props.customprimarycolor.r, props.customprimarycolor.g, props.customprimarycolor.b)
+		end
+		if props.customsecondarycolor then
+			SetVehicleCustomSecondaryColour(vehicle, props.customsecondarycolor.r, props.customsecondarycolor.g, props.customsecondarycolor.b)
+		end
+		if props.interiorColor then
+            SetVehicleInteriorColor(vehicle, props.interiorColor)
+        end
+		if props.dashboardColor then
+            SetVehicleDashboardColour(vehicle, props.dashboardColor)
+        end
+		if props.pearlescentColor and props.wheelColor then
+			SetVehicleExtraColours(vehicle, props.pearlescentColor, props.wheelColor)
+		end
+		if props.tyreSmokeColor then
+            SetVehicleTyreSmokeColor(vehicle, props.tyreSmokeColor[1], props.tyreSmokeColor[2], props.tyreSmokeColor[3])
+        end
+		-- wheels:
+		if props.wheels then
+            SetVehicleWheelType(vehicle, props.wheels)
+        end
+		-- windows:
+		if props.windowTint then
+            SetVehicleWindowTint(vehicle, props.windowTint)
+        end
+		-- neonlight:
+		if props.neonEnabled then
+            SetVehicleNeonLightEnabled(vehicle, 0, props.neonEnabled[1])
+            SetVehicleNeonLightEnabled(vehicle, 1, props.neonEnabled[2])
+            SetVehicleNeonLightEnabled(vehicle, 2, props.neonEnabled[3])
+            SetVehicleNeonLightEnabled(vehicle, 3, props.neonEnabled[4])
+        end
+		if props.neonColor then
+            SetVehicleNeonLightsColour(vehicle, props.neonColor[1], props.neonColor[2], props.neonColor[3])
+        end
+		-- mods:
+		if props.modSpoilers then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 0, props.modSpoilers, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 0, props.modSpoilers, false)
+			end
+        end
+		if props.modFrontBumper then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 1, props.modFrontBumper, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 1, props.modFrontBumper, false)
+			end
+        end
+		if props.modRearBumper then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 2, props.modRearBumper, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 2, props.modRearBumper, false)
+			end
+        end
+		if props.modSideSkirt then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 3, props.modSideSkirt, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 3, props.modSideSkirt, false)
+			end
+        end
+		if props.modExhaust then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 4, props.modExhaust, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 4, props.modExhaust, false)
+			end
+        end
+		if props.modFrame then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 5, props.modFrame, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 5, props.modFrame, false)
+			end
+        end
+		if props.modGrille then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 6, props.modGrille, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 6, props.modGrille, false)
+			end
+        end
+		if props.modHood then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 7, props.modHood, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 7, props.modHood, false)
+			end
+        end
+		if props.modFender then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 8, props.modFender, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 8, props.modFender, false)
+			end
+        end
+		if props.modRightFender then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 9, props.modRightFender, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 9, props.modRightFender, false)
+			end
+        end
+		if props.modRoof then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 10, props.modRoof, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 10, props.modRoof, false)
+			end
+        end
+		if props.modEngine then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 11, props.modEngine, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 11, props.modEngine, false)
+			end
+        end
+		if props.modBrakes then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 12, props.modBrakes, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 12, props.modBrakes, false)
+			end
+        end
+		if props.modTransmission then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 13, props.modTransmission, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 13, props.modTransmission, false)
+			end
+        end
+		if props.modHorns then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 14, props.modHorns, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 14, props.modHorns, false)
+			end
+        end
+		if props.modSuspension then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 15, props.modSuspension, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 15, props.modSuspension, false)
+			end
+        end
+		if props.modArmor then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 16, props.modArmor, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 16, props.modArmor, false)
+			end
+        end
+		if props.modTurbo then
+            ToggleVehicleMod(vehicle, 18, props.modTurbo)
+        end
+		if props.modSmokeEnabled then
+            ToggleVehicleMod(vehicle, 20, props.modSmokeEnabled)
+        end
+		if props.modXenon then
+            ToggleVehicleMod(vehicle, 22, props.modXenon)
+        end
+		if props.modFrontWheels then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 23, props.modFrontWheels, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 23, props.modFrontWheels, false)
+			end
+        end
+		if props.modBackWheels then
+            if props.modCustomTiresR then
+				SetVehicleMod(vehicle, 24, props.modBackWheels, props.modCustomTiresR)
+			else
+				SetVehicleMod(vehicle, 24, props.modBackWheels, false)
+			end
+        end
+		if props.modPlateHolder then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 25, props.modPlateHolder, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 25, props.modPlateHolder, false)
+			end
+        end
+		if props.modVanityPlate then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 26, props.modVanityPlate, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 26, props.modVanityPlate, false)
+			end
+        end
+		if props.modTrimA then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 27, props.modTrimA, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 27, props.modTrimA, false)
+			end
+        end
+		if props.modOrnaments then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 28, props.modOrnaments, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 28, props.modOrnaments, false)
+			end
+        end
+		if props.modDashboard then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 29, props.modDashboard, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 29, props.modDashboard, false)
+			end
+        end
+		if props.modDial then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 30, props.modDial, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 30, props.modDial, false)
+			end
+        end
+		if props.modDoorSpeaker then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 31, props.modDoorSpeaker, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 31, props.modDoorSpeaker, false)
+			end
+        end
+		if props.modSeats then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 32, props.modSeats, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 32, props.modSeats, false)
+			end
+        end
+		if props.modSteeringWheel then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 33, props.modSteeringWheel, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 33, props.modSteeringWheel, false)
+			end
+        end
+		if props.modShifterLeavers then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 34, props.modShifterLeavers, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 34, props.modShifterLeavers, false)
+			end
+        end
+		if props.modAPlate then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 35, props.modAPlate, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 35, props.modAPlate, false)
+			end
+        end
+		if props.modSpeakers then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 36, props.modSpeakers, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 36, props.modSpeakers, false)
+			end
+        end
+		if props.modTrunk then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 37, props.modTrunk, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 37, props.modTrunk, false)
+			end
+        end
+		if props.modHydrolic then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 38, props.modHydrolic, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 38, props.modHydrolic, false)
+			end
+        end
+		if props.modEngineBlock then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 39, props.modEngineBlock, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 39, props.modEngineBlock, false)
+			end
+        end
+		if props.modAirFilter then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 40, props.modAirFilter, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 40, props.modAirFilter, false)
+			end
+        end
+		if props.modStruts then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 41, props.modStruts, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 41, props.modStruts, false)
+			end
+        end
+		if props.modArchCover then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 42, props.modArchCover, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 42, props.modArchCover, false)
+			end
+        end
+		if props.modAerials then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 43, props.modAerials, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 43, props.modAerials, false)
+			end
+        end
+		if props.modTrimB then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 44, props.modTrimB, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 44, props.modTrimB, false)
+			end
+        end
+		if props.modTank then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 45, props.modTank, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 45, props.modTank, false)
+			end
+        end
+		if props.modWindows then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 46, props.modWindows, props.modCustomTiresF)
+			else
+				SetVehicleMod(vehicle, 46, props.modWindows, false)
+			end
+        end
+		if props.modLivery then
+            if props.modCustomTiresF then
+				SetVehicleMod(vehicle, 48, props.modLivery, props.modCustomTiresF)
+				SetVehicleLivery(vehicle, props.modLivery)
+			else
+				SetVehicleMod(vehicle, 48, props.modLivery, false)
+				SetVehicleLivery(vehicle, props.modLivery)
+			end
+        end
+		-- extras:
+		if props.extras then
+            for id, enabled in pairs(props.extras) do
+                if enabled then
+                    SetVehicleExtra(vehicle, tonumber(id), 0)
+                else
+                    SetVehicleExtra(vehicle, tonumber(id), 1)
+                end
+            end
+        end
+		-- stats:
+		if props.health then
+            SetEntityHealth(vehicle, props.health + 0.0)
+        end
+		if props.bodyHealth then
+            SetVehicleBodyHealth(vehicle, props.bodyHealth + 0.0)
+        end
+		if props.engineHealth then
+            SetVehicleEngineHealth(vehicle, props.engineHealth + 0.0)
+        end
+		if props.engineHealth and renderScorched and props.engineHealth < -3999.0 then
+			TriggerServerEvent('MojiaGarages:server:renderScorched', NetworkGetNetworkIdFromEntity(vehicle), true)
+		end
+		if props.tankHealth then
+            SetVehiclePetrolTankHealth(vehicle, props.tankHealth + 0.0)
+        end
+		if props.tankHealth and renderScorched and props.tankHealth < -999.0 then
+			TriggerServerEvent('MojiaGarages:server:renderScorched', NetworkGetNetworkIdFromEntity(vehicle), true)
+		end
+		if props.dirtLevel then
+            SetVehicleDirtLevel(vehicle, props.dirtLevel + 0.0)
+        end
+		if props.fuelLevel then
+            SetVehicleFuelLevel(vehicle, props.fuelLevel + 0.0)
+        end
+		-- doors:
+		if props.doorsmissing then
+            for id, state in pairs(props.doorsmissing) do
+				if state then
+					SetVehicleDoorBroken(vehicle, tonumber(id), state)
+					
+				end
+			end
+        end
+		-- tires
+		SetVehicleTyresCanBurst(vehicle, not props.bulletprooftires)
+		if not props.bulletprooftires and props.tiresburst then
+			for id, state in pairs(props.tiresburst) do
+				SetVehicleTyreBurst(vehicle, tonumber(id), state, 1000.0)
+			end
+		end
+		-- windows:
+		if props.windowsbroken then
+            for id, state in pairs(props.windowsbroken) do
+				if not state then
+					SmashVehicleWindow(vehicle, tonumber(id))
+				end
+			end
+        end
+		-- xenon lights:
+		if props.xenonColor then
+            SetVehicleXenonLightsColor(vehicle, props.xenonColor)
+        end
+	end
+end
